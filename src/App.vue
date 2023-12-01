a79af80f31c705db5bb0e89ef0f26186faa2aeff
Hesham Omar <heshamomar820@gmail.com>
2023-12-01 20:34:00 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<!-- src/App.vue -->
+
+<template>
+  <main class="w-full h-screen bg-opacity-75 flex justify-center items-center p-4">
+    <div class="max-w-xl w-full bg-gray-700 bg-opacity-90 flex flex-col justify-center items-center py-10 rounded-3xl">
+      <form @submit.prevent="submitForm" autocomplete="on" class="flex flex-col w-full text-white px-8">
+        <label for="name" class="w-full text-2xl mb-2">Name:</label>
+        <input type="text" class="text-lg text-black h-8 pl-2 mb-3 rounded-sm focus:outline-none focus:shadow-outline" placeholder="Ex: Hesham" v-model="formData.name" required>
+
+        <label for="email" class="w-full text-2xl mb-2">Email:</label>
+        <input type="email" id="email" class="text-lg text-black h-8 pl-2 mb-3 rounded-sm focus:outline-none focus:shadow-outline" placeholder="Ex: hesham@test.com" v-model="formData.email" required>
+
+        <label for="country" class="w-full text-2xl mb-2">Country:</label>
+        <CountryDropdown v-model="formData.country" />
+
+        <button class=" focus:outline-none border border-transparent text-xl py-3 px-5 rounded-lg shadow-sm text-center text-social-500 hover:bg-white bg-transparent hover:text-gray-700 border-white font-medium" type="submit">
+          <span v-if="!loading">
+            Submit
+          </span>
+          <span v-if="loading" class="animate-spin inline-block w-6 h-6 border-4 border-solid border-r-transparent rounded-full"></span>
+        </button>
+        
+      </form>
+    </div>
+  </main>
+
+</template>
+
+<script>
+import CountryDropdown from './components/CountryDropdown.vue';
+
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        country: '',
+      },
+      loading: false,
+    };
+  },
+  methods: {
+    async submitForm() {
+      this.loading = true;
+      fetch('https://sellenvo.com/api/test', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json', 
+        },
+        body: this.formData,
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.loading = false;
+        this.$swal({
+          title: "Submitted!",
+          text: "We've received your data!",
+          icon: "success"
+        });
+      })
+      .catch(error => {
+        console.error(error);
+        this.loading = false;
+        this.$swal({
+          title: "Error!",
+          text: "There is an error!",
+          icon: "error",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      });
+    },
+    showAlert() {
+      this.$swal({
+        title: "Good job!",
+        text: "You clicked the button!",
+        icon: "success"
+      });
+    },
+  },
+  components: {
+    CountryDropdown,
+  },
+};
+
+</script>
+
+
+<style scoped>
+  main {
+    background: url("./map3.jpg");
+    background-size: cover;
+    background-position: center;
+  }
+</style>
---
+++ src/components/CountryDropdown.vue
@@ -0,0 +1,38 @@
+<!-- src/components/CountryDropdown.vue -->
+
+<template>
+    <select class="text-lg text-black pl-2 mb-5 h-8 rounded-md focus:outline-none focus:shadow-outline" v-bind="selectedCountry" required>
+        <option value="" disabled selected hidden>Choose a country</option>
+        <option v-for="country in countries" :key="country.code" :value="country.name">
+            {{ country.name }}
+        </option>
+    </select>
+</template>
+
+<script >
+import {countries} from 'i18n-iso-countries/langs/en.json';
+
+
+export default {
+    props: {
+        selectedCountry: String,
+    },
+    data() {
+        return {
+            countries: {},
+        };
+    },
+    mounted() {
+        this.fetchingCountryNames();
+    },
+    methods: {
+        fetchingCountryNames() {
+            this.countries = Object.keys(countries).map((key) => ({ key, name: this.modelingCountryName(countries[key])}))
+            console.log(this.countries)
+        },
+        modelingCountryName(name) { 
+            return typeof name === 'string' ? name : name[name.length - 1]
+        }
+    },
+};
+</script>
